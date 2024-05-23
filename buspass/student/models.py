90b53efab61413285042ad88cefeffaf33b89321
Srishti <131654187+13-Srishti@users.noreply.github.com>
2024-05-23 15:57:11 +0530

    Update models.py

--- buspass/student/models.py
+++ buspass/student/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Studentdata(models.Model):
+    id= models.AutoField
+    eno= models.CharField(max_length=20, default='')
+    name= models.CharField(max_length=50, default='')
+    year= models.IntegerField(default=1)
+    branch= models.CharField(max_length=50, default='')
+
+    def __str__(self):
+        return self.eno
