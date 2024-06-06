d8b5f34e972c03bff42abb1a93ba1d08c58e8842
Srishti <131654187+13-Srishti@users.noreply.github.com>
2024-06-06 16:52:33 +0530

    Update models.py

--- buspass/student/models.py
+++ buspass/student/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 class Studentdata(models.Model):
-    id= models.AutoField
+   id= models.AutoField
     eno= models.CharField(max_length=20, default='')
     name= models.CharField(max_length=50, default='')
-    year= models.IntegerField(default=1)
-    branch= models.CharField(max_length=50, default='')
+    sem= models.CharField(max_length=2, default='')
+    course= models.CharField(max_length=50, default='')
+    add= models.CharField(max_length=50, default='')
+    phone= PhoneNumberField(max_length=10, default='')
 
     def __str__(self):
         return self.eno
