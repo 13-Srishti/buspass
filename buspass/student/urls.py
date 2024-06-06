03ecf9c81f249c76e45fbf8dd98548ef0a809e77
Srishti <131654187+13-Srishti@users.noreply.github.com>
2024-06-06 16:51:46 +0530

    Update urls.py

--- buspass/student/urls.py
+++ buspass/student/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('stu/', views.stu, name='stu'),
+    path('apply/', views.apply, name='apply'),
+    path('chkst/', views.chkst, name='chkst')
 ]
